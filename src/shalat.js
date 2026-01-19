8b40aa8653ead63a612239bc128d60cb577a0380
manabumantappu <azishachigo@gmail.com>
2026-01-19 21:49:08 +0700

    Update shalat.js

--- src/shalat.js
+++ src/shalat.js
@@ -1,20 +1,21 @@
-export function getShalatTimes(lat, lng) {
-const times = {
-Subuh: '04:35',
-Dzuhur: '12:00',
-Ashar: '15:20',
-Maghrib: '18:00',
-Isya: '19:10'
-};
+export function getShalatTimes() {
+  const times = {
+    Subuh: '04:35',
+    Dzuhur: '12:00',
+    Ashar: '15:20',
+    Maghrib: '18:00',
+    Isya: '19:10'
+  };
 
+  const box = document.getElementById('shalat-times');
+  box.innerHTML = '';
 
-const container = document.getElementById('shalat-times');
-container.innerHTML = '';
-
-
-Object.keys(times).forEach(name => {
-const div = document.createElement('div');
-div.innerHTML = `<strong>${name}</strong>: ${times[name]}`;
-container.appendChild(div);
-});
+  Object.entries(times).forEach(([name, time]) => {
+    box.innerHTML += `
+      <div class="bg-white p-3 rounded shadow text-center">
+        <div class="font-bold">${name}</div>
+        <div class="text-lg">${time}</div>
+      </div>
+    `;
+  });
 }
