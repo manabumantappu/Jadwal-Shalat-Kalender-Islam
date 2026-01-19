275d64d01478f5c32b1b59876da922f7e836cf1c
manabumantappu <azishachigo@gmail.com>
2026-01-19 21:10:32 +0700

    Update hijri.js

--- src/hijri.js
+++ src/hijri.js
@@ -1,3 +1,18 @@
+// Kalender Hijriyah otomatis (Ummul Qura / Hisab)
+export function getHijriDate() {
+const today = new Date();
+
+
+const hijri = new Intl.DateTimeFormat('id-TN-u-ca-islamic', {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+}).format(today);
+
+
+document.getElementById('hijri-date').innerText = hijri;
+}
+```javascript
 export function getHijriDate() {
 const hijri = '10 Ramadhan 1447 H';
 document.getElementById('hijri-date').innerText = hijri;
