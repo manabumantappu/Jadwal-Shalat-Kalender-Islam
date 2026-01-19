52113aee7c15421d0ee475619947abc175846380
manabumantappu <azishachigo@gmail.com>
2026-01-19 22:18:20 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// === KALENDER HARI INI ===
+const today = new Date();
+
+// MASEHI
+const masehi = today.toLocaleDateString("id-ID", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+});
+
+// HIJRIAH
+const hijriah = today.toLocaleDateString(
+  "id-ID-u-ca-islamic",
+  {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }
+);
+
+// JEPANG (WAREKI)
+const jepang = today.toLocaleDateString(
+  "ja-JP-u-ca-japanese",
+  { year: "numeric" }
+);
+
+// TAMPILKAN KE HTML
+document.getElementById("masehi").innerText = masehi;
+document.getElementById("hijriah").innerText = hijriah;
+document.getElementById("jepang").innerText = jepang;
